50d229d631ce083b3694dc4c06b86a0aaadf298c
Jefferson Ferreira <jefferson_lf@live.com>
2023-02-23 21:22:35 -0300

    feat: style toggle menu

--- higlimp_site/src/components/Header/index.jsx
+++ higlimp_site/src/components/Header/index.jsx
@@ -3,9 +3,46 @@ import styles from "./styles.module.css";
 import Dropdown from "react-bootstrap/Dropdown";
 
 import { GoThreeBars } from "react-icons/go";
-import React from "react";
+import React, { useState } from "react";
 
 export const Header = () => {
+
+  const [isHoveringA, setIsHoveringA] = useState(false);
+  const [isHoveringB, setIsHoveringB] = useState(false);
+  const [isHoveringC, setIsHoveringC] = useState(false);
+  const [isHoveringD, setIsHoveringD] = useState(false);
+
+  const handleMouseEnterA = () => {
+    setIsHoveringA(true);
+  };
+
+  const handleMouseLeaveA = () => {
+    setIsHoveringA(false);
+  };
+
+  const handleMouseEnterB = () => {
+    setIsHoveringB(true);
+  };
+
+  const handleMouseLeaveB = () => {
+    setIsHoveringB(false);
+  };
+
+  const handleMouseEnterC = () => {
+    setIsHoveringC(true);
+  };
+
+  const handleMouseLeaveC = () => {
+    setIsHoveringC(false);
+  };
+
+  const handleMouseEnterD = () => {
+    setIsHoveringD(true);
+  };
+
+  const handleMouseLeaveD = () => {
+    setIsHoveringD(false);
+  };
 
     const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
         <button
@@ -22,11 +59,30 @@ export const Header = () => {
         </button>
       ));
 
+      const CustomMenu = React.forwardRef(
+        ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+          
+      
+          return (
+            <div
+              ref={ref}
+              style={{background: 'var(--cadet)', right: '0rem', top: '2rem'}}
+              className={className}
+              aria-labelledby={labeledBy}
+            >
+              <ul className="list-unstyled">
+                {React.Children.toArray(children)}
+              </ul>
+            </div>
+          );
+        },
+      );
+
   return (
     <header className={styles.container}>
       <div className={styles.content}>
         <div>HIG LIMP</div>
-        {window.screen.width >= 600 ? (
+        {window.screen.width <= 600 ? (
           <nav>
             <ButtonC background="green" title="Início"></ButtonC>
             <ButtonC
@@ -40,14 +96,42 @@ export const Header = () => {
         ) : (
           <Dropdown>
             <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-              <GoThreeBars size={20} color={'white'}/>
+              <GoThreeBars size={20} color={'var(--orange)'}/>
             </Dropdown.Toggle>
 
-            <Dropdown.Menu >
-              <Dropdown.Item href="#/action-1">Início</Dropdown.Item>
-              <Dropdown.Item href="#services">Serviços</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Sobre nós</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Contato</Dropdown.Item>
+            <Dropdown.Menu as={CustomMenu}>
+              <Dropdown.Item 
+                style={{color: 'white', background: isHoveringA ? 'var(--orange)' : 'var(--cadet)'}} 
+                onMouseEnter={handleMouseEnterA}
+                onMouseLeave={handleMouseLeaveA}
+                href="#/action-1" 
+              >
+                Início
+              </Dropdown.Item>
+              <Dropdown.Item 
+                style={{color: 'white', background: isHoveringB ? 'var(--orange)' : 'var(--cadet)'}} 
+                onMouseEnter={handleMouseEnterB}
+                onMouseLeave={handleMouseLeaveB}
+                href="#services"
+              >
+                Serviços
+              </Dropdown.Item>
+              <Dropdown.Item 
+                style={{color: 'white', background: isHoveringC ? 'var(--orange)' : 'var(--cadet)'}}
+                onMouseEnter={handleMouseEnterC}
+                onMouseLeave={handleMouseLeaveC}
+                href="#/action-3"
+              >
+                Sobre nós
+              </Dropdown.Item>
+              <Dropdown.Item 
+                style={{color: 'white', background: isHoveringD ? 'var(--orange)' : 'var(--cadet)'}}
+                onMouseEnter={handleMouseEnterD}
+                onMouseLeave={handleMouseLeaveD}
+                href="#/action-3"
+              >
+                Contato
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         )}
--- higlimp_site/src/components/ImgPresentation/index.jsx
+++ higlimp_site/src/components/ImgPresentation/index.jsx
@@ -1,10 +1,11 @@
 import foto1 from '../../assets/layout/HeaderImg.jpg'
+import styles from './styles.module.css'
 
 export const ImgPresentation = () => {
 
     return(
         <div>
-            <img src={foto1} alt='sofa com titulo' style={{width: "100vw"}}></img>
+            <img src={foto1} alt='sofa com titulo' style={{width: "100vw"}} className={styles.headerImg}></img>
         </div>
     )
 }
